977e2332cdf3660574c978c5417b2cc379621376
ThomasGill13 <26653775+ThomasGill13@users.noreply.github.com>
2022-11-26 12:19:52 +0000

    Added controls for main layout

--- DnDBattleClock/Form1.Designer.cs
+++ DnDBattleClock/Form1.Designer.cs
@@ -31,12 +31,18 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InitiativeBonus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InitRoll = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InitScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InitiativeRank = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PlayerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.InitBonus = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.InitiativeRoll = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.InitiativeBonus = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.InitiativeRank = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -58,17 +64,41 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn2,
             this.InitiativeBonus,
+            this.InitRoll,
+            this.InitScore,
             this.InitiativeRank});
             this.dataGridView1.Location = new System.Drawing.Point(97, 61);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(453, 227);
+            this.dataGridView1.Size = new System.Drawing.Size(545, 227);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridViewTextBoxColumn2
             // 
             this.dataGridViewTextBoxColumn2.HeaderText = "Player Name";
             this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // InitiativeBonus
+            // 
+            this.InitiativeBonus.HeaderText = "Initiative Bonus";
+            this.InitiativeBonus.Name = "InitiativeBonus";
+            // 
+            // InitRoll
+            // 
+            this.InitRoll.HeaderText = "Initiative Roll";
+            this.InitRoll.Name = "InitRoll";
+            // 
+            // InitScore
+            // 
+            this.InitScore.HeaderText = "Initiative Score";
+            this.InitScore.Name = "InitScore";
+            this.InitScore.ReadOnly = true;
+            // 
+            // InitiativeRank
+            // 
+            this.InitiativeRank.HeaderText = "Initiative Rank";
+            this.InitiativeRank.Name = "InitiativeRank";
+            this.InitiativeRank.ReadOnly = true;
             // 
             // PlayerName
             // 
@@ -90,24 +120,54 @@
             this.dataGridViewTextBoxColumn1.HeaderText = "Player Name";
             this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
             // 
-            // InitiativeBonus
+            // button1
             // 
-            this.InitiativeBonus.HeaderText = "Initiative Bonus";
-            this.InitiativeBonus.Name = "InitiativeBonus";
+            this.button1.Location = new System.Drawing.Point(35, 508);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Players";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // InitiativeRank
+            // button2
             // 
-            this.InitiativeRank.HeaderText = "Initiative Rank";
-            this.InitiativeRank.Name = "InitiativeRank";
+            this.button2.Location = new System.Drawing.Point(596, 508);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Combat";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(412, 508);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Update Initiative";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(224, 508);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 42);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save Players";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 617);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "D&D Battle Clock";
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -118,12 +178,18 @@
 
         private GroupBox groupBox1;
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private DataGridViewTextBoxColumn PlayerName;
         private DataGridViewTextBoxColumn InitBonus;
         private DataGridViewTextBoxColumn InitiativeRoll;
         private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private DataGridViewTextBoxColumn InitiativeBonus;
+        private DataGridViewTextBoxColumn InitRoll;
+        private DataGridViewTextBoxColumn InitScore;
         private DataGridViewTextBoxColumn InitiativeRank;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
